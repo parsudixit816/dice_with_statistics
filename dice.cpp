fb4b6eb0520f38bc31aa02442f758b49edf263f5
parsudixit816 <267361320+parsudixit816@users.noreply.github.com>
2026-09-22 13:29:06 +0530

    2 player added

--- dice.cpp
+++ dice.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int num,md,rn,r1,r2,r3,lc;
 int nroll=0;
-int troll=0;
+int troll=0,troll1=0;
 int roll=-1;
 string input ;
 bool run=true;
-
+bool pl=0;
 
 
 void start(){
@@ -49,12 +49,12 @@ void grid(){
 }
 
 /////////////
-float one=0;
-float two=0;
-float thr=0;
-float fur=0;
-float fiv=0;
-float six=0;
+float one=0,one1=0,
+        two=0,two1=0,
+        thr=0,thr1=0,
+        fur=0,fur1=0,
+        fiv=0,fiv1=0,
+        six=0,six1=0;
 
 void statcount(){ num=rn; if (num==1){ one++; }
                             else if (num==2){ two++; }
@@ -66,7 +66,7 @@ void statcount(){ num=rn; if (num==1){ one++; }
 /////////////
 
 void statprint(){
-                cout<<endl;
+                cout<<endl<<"# player 1 :"<<endl;
                 cout<<"(1)--> "<<one<<"/"<<troll<<"         "<<one/troll*100<<"%"<<endl;
                 cout<<"(2)--> "<<two<<"/"<<troll<<"         "<<two/troll*100<<"%"<<endl;
                 cout<<"(3)--> "<<thr<<"/"<<troll<<"         "<<thr/troll*100<<"%"<<endl;
@@ -75,6 +75,26 @@ void statprint(){
                 cout<<"(6)--> "<<six<<"/"<<troll<<"         "<<six/troll*100<<"%"<<endl;
 
 } 
+
+void statcount1(){ num=rn; if (num==1){ one1++; }
+                            else if (num==2){ two1++; }
+                            else if (num==3){ thr1++; }
+                            else if (num==4){ fur1++; }
+                            else if (num==5){ fiv1++; }
+                            else if (num==6){ six1++; };
+}
+
+void statprint1(){
+                cout<<endl<<"# player 2 :"<<endl;
+                cout<<"(1)--> "<<one1<<"/"<<troll1<<"         "<<one1/troll1*100<<"%"<<endl;
+                cout<<"(2)--> "<<two1<<"/"<<troll1<<"         "<<two1/troll1*100<<"%"<<endl;
+                cout<<"(3)--> "<<thr1<<"/"<<troll1<<"         "<<thr1/troll1*100<<"%"<<endl;
+                cout<<"(4)--> "<<fur1<<"/"<<troll1<<"         "<<fur1/troll1*100<<"%"<<endl;
+                cout<<"(5)--> "<<fiv1<<"/"<<troll1<<"         "<<fiv1/troll1*100<<"%"<<endl;
+                cout<<"(6)--> "<<six1<<"/"<<troll1<<"         "<<six1/troll1*100<<"%"<<endl;
+
+} 
+
 
 
 void inputr(){
@@ -103,7 +123,33 @@ void inputr(){
     };
 }
 
-void twoinputr(){}
+void twoinputr(){
+    
+    statprint();statprint1();
+    cout<<"-------------------------------------------------------"<<endl; 
+    cout<<"# player "<< pl+1 <<" :"<<endl;   
+    cout<<"# press enter to roll the dice (or number of the times you want):";
+    getline(cin,input);
+
+    if (input==""){ input="1"; };
+    
+    if (input=="q" || input=="Q"){ cout<<endl<<"quiting..."<<endl; run=false; }
+
+    else { nroll = stoi(input); };
+  
+    if (nroll > 0 ){
+            
+        for (roll = 1; roll <= nroll; roll++){
+
+            rn=rand()%6+1;
+             
+            if (!pl) { statcount(); troll++; pl=1; }
+            else if (pl) { statcount1(); troll1++; pl=0;}
+
+        };
+    };
+
+}
 
 void moad1(){
 
@@ -121,19 +167,21 @@ void moad1(){
 }
 
 
-void moad2(){ cout<<"";
+void moad2(){
             
     system("clear");
     cout<<"==========================================================="<<endl<<"### Dice player"<<endl<<endl;
     cout<<"------------------------------------------------------------"<<endl;
     cout<<"# mode 2) Two_player. "<<endl;
-    cout<<"-----------------------------------------------------------";
-    cout<<"-------------------under devlopment------------------------";
+    cout<<"-----------------------------------------------------------"<<endl;
+    
     grid();
     
     cout<<"-----------------------------------------------------------";
     twoinputr(); 
-            }
+
+
+}
 
 
 int main(){
@@ -146,7 +194,8 @@ start();
                 while (input!="0" && run==true); 
                 }
 
-     else if (md==2) { moad2(); }
+     else if (md==2) { do {  moad2();  }
+                while (input!="0" && run==true); }
 
     else { cout<<" #### not avlable ! "; };
 
